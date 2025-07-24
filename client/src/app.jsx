869901f8329ef4d554d056872dc911b49bb67b7a
wiidu <wiiidu315@gmail.com>
2025-07-24 11:20:17 -0500

    Installed socket.io-client and added BrowserRouter wrapper

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import socket from 'sockets/socket';
 import Login from 'pages/login';
 
 // We'll import needed pages/components as we develop and push more code
 
 function App() {
+    useEffect( () => {
+        socket.connect();
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={Login} /> {/* This is placeholder code, we can delete the import and this route later */}
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={Login} /> {/* This is placeholder code, we can modify the Login import and this route later */}
+            </Routes>
+        </BrowserRouter>
     )
 }
+
+export default App;
