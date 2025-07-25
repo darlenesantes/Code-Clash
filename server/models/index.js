fe093267dea61e09ca3a6d89df592a57cf5daa64
Darlene Santes <darlenesantes@gmail.com>
2025-07-25 15:03:02 -0500

    Add Problem model definition and update index.js to include it

--- server/models/index.js
+++ server/models/index.js
@@ -4,19 +4,22 @@
  * This file initializes the Sequelize models for the application
  * so that they can be used throughout the app
  * 
- * We are importing the user model here
+ * We are importing the user and problem models here
  */
 
 const sequelize = require('./database');
 const defineUser = require('./user');
+const defineProblem = require('./problem'); // Import the problem model
 
-// Initialize the User model with the sequelize instance
+// Initialize the User model and Problem model with the sequelize instance
 const User = defineUser(sequelize);
+const Problem = defineProblem(sequelize); 
 
 // Export the models
 module.exports = {
     sequelize,
-    User
+    User,
+    Problem
 };
 
 
