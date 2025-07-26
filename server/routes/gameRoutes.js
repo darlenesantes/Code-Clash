9c2ab507a755b510a0c0a9aae4f415b7337970a6
Darlene Santes <darlenesantes@gmail.com>
2025-07-26 10:15:06 -0500

    Add initial game routes file with documentation

---
+++ server/routes/gameRoutes.js
@@ -0,0 +1,8 @@
+/**
+ * Game Routes
+ * This file contains routes for game-related actions
+ * These routes are:
+ * - Starting a new game
+ * - Randomly selecting a problem for the game
+ * - Submitting a solution to the problem --> TODO: implement tomorrow
+ */
