0a193f59d42bc6f9ca36f11fd94cb888419dfdf0
Darlene Santes <darlenesantes@gmail.com>
2025-07-26 11:50:04 -0500

    Add user and game routes to app.js; update gameRoutes.js documentation

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -2,7 +2,13 @@
  * Game Routes
  * This file contains routes for game-related actions
  * These routes are:
- * - Starting a new game
+ * - Starting a new game -> I don't think this is needed since this logic is handled by socket.io
  * - Randomly selecting a problem for the game
  * - Submitting a solution to the problem --> TODO: implement tomorrow
  */
+
+const express = require("express");
+const router = express.Router();
+
+// this route will be used to start a new game
+// waiting on problems 
